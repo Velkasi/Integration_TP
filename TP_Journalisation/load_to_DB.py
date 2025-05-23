7b31d8862ac8e998f081b2a808b04fb1520989a4
Velkasi <ivfrvek@gmail.com>
2025-05-23 16:33:33 +0200

    Tp Fini

--- TP_Journalisation/load_to_DB.py
+++ TP_Journalisation/load_to_DB.py
@@ -1,6 +1,9 @@
-import datetime
 import pandas as pandas
 import pymysql
+
+from TP_Journalisation import logging_generation
+from TP_Journalisation.logging_generation import ini_log_file, log_ignored_row, log_insert_success, log_error, \
+    close_log_file
 
 # Dataframe a partir du CSV
 df=pandas.read_csv('customer.csv')
@@ -19,52 +22,62 @@ connection = pymysql.connect(
 cursor = connection.cursor()
 
 #Initialiser le fichier de journalisation
-def ini_log_file():
-    #On donne un nom au fichier contenant date et heure
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    filename = f'log_{timestamp}'
-
-    #Ouvrir le fichier en mode ecriture
-    log_file = open(filename, "w", encoding="utf-8")
-
-    #En-tete
-    log_file.write("Log de migration de donnees \n")
-    log_file.write(f'Date : {datetime.datetime.now()}')
-
-    return log_file
+log_file = ini_log_file()
 
 # Traitement ligne par ligne du CSV
+for _, row in df.iterrows():
+    logging_generation.total_rows += 1
 
+    try :
 ######################### AVANT MIGRATION #########################
-
 
 # Verifier les donnees inssentielles de la base initiales avec insertion
 # Si un champ obligatoire est manquant, on le consigne dans les logs
-
+        if pandas.isnull(row['email']):
+            log_ignored_row(log_file, row, 'required field email is missing')
+            continue
 
 # Insertion les donnees dans la table
+        cursor.execute("""
+            INSERT INTO customers (name, email, birthdate, total_amount, enabled)
+            VALUES (%s, %s, %s, %s, %s)
+            """, (
+            row["nom"], #Nom client
+            row["email"], # Email client
+            row["date_naissance"], # Date de naissance
+            row["montant"], # Montant total
+            bool(row["actif"]))) # conversion explicit en booleen
 
 ######################### PENDANT MIGRATION #########################
-
-
 # Si insertion reussie : Log l'evenement
+        log_insert_success(log_file, row)
 
 
 # Si erreur : Log erreur
+    except Exception as e:
+        log_error(log_file, row, str(e))
 
 ######################### APRES MIGRATION #########################
 
 # Verification du fichier
-
+# Verification s'il y a des dates futures (incorrectes)
+cursor.execute("""
+    SELECT COUNT(*) FROM customers
+    WHERE birthdate > CURDATE();
+""")
+future_birthdates = cursor.fetchone()[0]
+print(f"{future_birthdates} clients ont une date de naissance future")
 
 # Fermeture fichier log
 
 
 # Generation du rapport final
+close_log_file(log_file)
 
 
 # Fermeture du cursor et de la connexion a la base
-
+cursor.close()
+connection.close()
 
 # Message de fin dans le terminal
 
--- TP_Journalisation/logging_generation.py
+++ TP_Journalisation/logging_generation.py
@@ -1,3 +1,4 @@
+import datetime
 # Variable globales de suivi
 
 total_rows = 0
@@ -8,6 +9,21 @@ error_rows = []
 
 
 # Initialiser le fichier de log
+def ini_log_file():
+    #On donne un nom au fichier contenant date et heure
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    filename = f'log_{timestamp}'
+
+    #Ouvrir le fichier en mode ecriture
+    log_file = open(filename, "w", encoding="utf-8")
+
+    #En-tete
+    log_file.write("Log de migration de donnees \n")
+    log_file.write(f'Date : {datetime.datetime.now()}')
+
+    return log_file
+
+
 def log_insert_success(log_file, row):
     global inserted_count
     inserted_count += 1
@@ -30,9 +46,30 @@ def log_error(log_file, row, error_msg):
         "error" : error_msg
     })
 
-    log_file.write(f'Error: {row.get('email', 'N/A')} | Reason : {error_msg}')
+    log_file.write(f'Error: {row.get('email', 'N/A')} | Reason : {error_msg}\n')
 
 # Generer un rapport de migration
+def generate_summary_report():
+    filename = "resume_migration.txt"
 
+    with open(filename, "w", encoding="utf-8") as report:
+        report.write("Rapport de migration\n")
+        report.write(f'Total de ligne traitées : {total_rows}\n')
+        report.write(f'Insertion reussies : {inserted_count}\n')
+        report.write(f'Lignes ignorees : {ignored_count}\n')
+        report.write(f"Erreurs : {error_count}\n\n")
+
+        #Detail des erreurs
+        if error_rows:
+            report.write("Detail des erreurs : \n")
+            for error in error_rows:
+                report.write(f'Email: {error['email']} Erreur: {error['error']}\n')
+        else :
+            report.write("Aucune erreur detectee")
+        report.close()
 
 # Fermer proprement le fichier log et generer le rapport final
+def close_log_file(log_file):
+    log_file.write('\nFin du journal')
+    log_file.close()
+    generate_summary_report()
