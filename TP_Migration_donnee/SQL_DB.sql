8c50c326543daab5c10a1620897e2e9dbf693022
Velkasi <ivfrvek@gmail.com>
2025-06-02 18:25:25 +0200

    Copier Coller et Creation BDD

--- TP_Migration_donnee/SQL_DB.sql
+++ TP_Migration_donnee/SQL_DB.sql
@@ -1,9 +1,13 @@
-CREATE DATABASE company_db;
-USE company_db
+CREATE DATABASE IF NOT EXISTS company_db;
+USE company_db;
 
-CREATE TABLE employees (
+CREATE TABLE IF NOT EXISTS employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (100),
     email VARCHAR (100),
     date_recrut DATETIME,
-    salaire_annuel DECIMAL(10,3)
-    );
+    salaire_annuel DECIMAL(10,2),
+    salarie_active BOOLEAN
+);
+
+DROP TABLE IF EXISTS employees;
